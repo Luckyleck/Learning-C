4acd5f58bfe9064a85d8dbb2f0071c892d17cf11
Alex <luckyleck29@gmail.com>
2023-12-15 13:54:35 -0500

    Logic Statements and Switches

--- 3.1-logic.c
+++ 3.1-logic.c
@@ -2,6 +2,64 @@
 #include <stdbool.h>
 
 int main() 
-{ 
+{
+    int age = 25;
+    double money = 30000;
+    if(age > 17 && money > 25000)
+    {
+        printf("This is adult\n");
+    }
+    else
+    {
+        printf("Not an adult\n");
+    }
+
+    /*
+    if()
+    {
+
+    }
+    elseif()
+    {
+
+    }
+    else()
+    {
+
+    }
+
+    if () printf(code); // YOU CAN ONLY PUT ONE STATEMENT AFTERWARDS
+
+    */
+
+    // Switch requires and integer based character, an int or a character
+
+    int menuChoice = 2;
+    switch(menuChoice)
+    {
+        case 0:
+            printf("Case 0\n");
+            break;
+        case 1:
+            printf("Case 1\n");
+            break;
+        case 2:
+            printf("Case 2\n");
+            break;
+        case 3:
+            printf("Case 3\n");
+            break;
+        default:
+            printf("Default cases");
+            break;
+    }
+
+    // switch(menuChoice)
+        // case 1:
+        // case 2:
+        // case 3:
+        // case 4:
+        //     printf("Hit 3 cases above as well");
+
     return 0;
 }
